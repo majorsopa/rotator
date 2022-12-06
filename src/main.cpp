d608f3ea85f62b34dec2e9c4afca56e40c69d395
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-12-06 18:52:31 -0500

    take range from args

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,23 @@ inline bool file_exists (const std::string& name) {
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         run_verification();
-    } else if (argc == 2) {
+    } else if (argc == 8) {
         if (!file_exists(argv[1])) {
             std::cout << "File does not exist" << std::endl;
             return 1;
         }
         std::cout << "starting search" << std::endl;
-        TextureFinder texture_finder = TextureFinder(argv[1], -10000, 10000, 10, 60, -10000, 10000, new Texture(TextureType::VANILLA));
+
+        int start_x = std::strtol(argv[2], nullptr, 10);
+        int end_x = std::strtol(argv[3], nullptr, 10);
+        int start_y = std::strtol(argv[4], nullptr, 10);
+        int end_y = std::strtol(argv[5], nullptr, 10);
+        int start_z = std::strtol(argv[6], nullptr, 10);
+        int end_z = std::strtol(argv[7], nullptr, 10);
+
+        std::cout << start_x << " " << end_x << " " << start_y << " " << end_y << " " << start_z << " " << end_z << std::endl;
+
+        TextureFinder texture_finder = TextureFinder(argv[1], start_x, end_x, start_y, end_y, start_z, end_z, new Texture(TextureType::VANILLA));
         texture_finder.run();
         std::cout << "done" << std::endl;
     }
